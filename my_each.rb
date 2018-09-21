8fa4c21d7746d77f7f1f434ef68d279c80be6f80
clifford <clifford@noukod.com>
2018-09-21 17:54:48 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,4 @@
-def my_each(collection) # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
   # i = 0
   # while i < array.length
@@ -6,8 +6,11 @@ def my_each(collection) # put argument(s) here
   #   i += 1
   # end
   # array
-  collection = [1, 2, 3, 4]
-
-my_each(collection) do |i|
-  puts i
+  i =0
+  while  i < self.length
+    yield self[i]
+    i += 1
+  end 
+    self[0]
+  end
 end
